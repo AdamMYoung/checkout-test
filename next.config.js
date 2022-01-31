2ff01838f60d454e8ad2e3574938a64cb6427975
Adam Young <adamyoungg97@gmail.com>
2022-01-31 16:25:59 +0000

    Added sentryclirc config

--- next.config.js
+++ next.config.js
@@ -13,9 +13,6 @@ const moduleExports = {
 
 const sentryWebpackPluginOptions = {
     silent: true,
-    authToken: process.env.SENTRY_AUTH_TOKEN,
-    org: process.env.SENTRY_ORG,
-    project: process.env.SENTRY_PROJECT,
 };
 
 module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
