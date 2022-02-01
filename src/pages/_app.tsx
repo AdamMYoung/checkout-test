b3d49a7ed9a1ef2bbb92503903caebaf5ec0fea6
Adam Young <adamyoungg97@gmail.com>
2022-02-01 16:37:00 +0000

    Added HTML lang to _app page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,20 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { Layout } from '../components';
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
-        <ChakraProvider>
-            <Layout>
-                <Component {...pageProps} />
-            </Layout>
-        </ChakraProvider>
+        <>
+            <Head>
+                <html lang="en" />
+            </Head>
+            <ChakraProvider>
+                <Layout>
+                    <Component {...pageProps} />
+                </Layout>
+            </ChakraProvider>
+        </>
     );
 }
 
