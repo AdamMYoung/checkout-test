bfdccd42da27b81d85f2d2da79cc821773247c6d
Adam Young <adamyoungg97@gmail.com>
2022-02-01 16:40:25 +0000

    Added 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,23 @@
+import { Box, Button, Center, Heading, Stack } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+
+const NotFound = () => {
+    const router = useRouter();
+
+    return (
+        <Box height="100vh">
+            <Center>
+                <Stack spacing="8">
+                    <Heading>The page you're looking for can't be found.</Heading>
+                    <Box>
+                        <Button colorScheme="green" onClick={() => router.replace('/')}>
+                            Go Home
+                        </Button>
+                    </Box>
+                </Stack>
+            </Center>
+        </Box>
+    );
+};
+
+export default NotFound;
